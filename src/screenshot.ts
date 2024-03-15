9fcbd1d03b6fc2e6cd3bf9ec64add48879cfbd4a
ofadiman <ofadiman@gmail.com>
2024-03-15 17:29:32 +0100

    feature: make code screenshot using puppeteer

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -1,25 +1,90 @@
-import { homedir } from 'node:os'
-import { join } from 'node:path'
-import { Browser, getInstalledBrowsers, install } from '@puppeteer/browsers'
+import puppeteer from 'puppeteer'
+import { bundledLanguages, bundledThemes, getHighlighter } from 'shiki'
 
+const template = (args: { code: string; padding: number }) => `<!doctype html>
+<html>
+  <head>
+    <title>nvim-code-screenshot</title>
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+      }
+
+      pre {
+        width: fit-content;
+        padding: ${args.padding}px;
+      }
+    </style>
+  </head>
+  <body>
+    ${args.code}
+  </body>
+</html>`
+
+// TODO: Add feedback saying, that the screenshot was made or that something went wrong.
 void (async () => {
-  const defaultPuppeteerCacheDir = join(homedir(), '.cache', 'puppeteer')
-  const installedBrowsers = await getInstalledBrowsers({
-    cacheDir: defaultPuppeteerCacheDir,
+  const highlighter = await getHighlighter({
+    themes: Object.keys(bundledThemes),
+    langs: Object.keys(bundledLanguages),
   })
 
-  console.info(installedBrowsers)
+  // TODO: Pass real code from nvim here.
+  const htmlCode = highlighter.codeToHtml(
+    `const highlighter = await getHighlighter({
+  themes: Object.keys(bundledThemes),
+  langs: Object.keys(bundledLanguages),
+});`,
+    {
+      // TODO: Figure out correct language mapping for current file.
+      lang: 'javascript',
+      // TODO: Allow users to configure theme.
+      theme: 'poimandres',
+    },
+  )
 
-  const hasZeroInstalledBrowsers = installedBrowsers.length === 0
-  if (hasZeroInstalledBrowsers) {
-    console.info(`Installing default browser for puppeteer`)
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  // TODO: Allow people to configure code padding.
+  await page.setContent(template({ padding: 10, code: htmlCode }))
 
-    await install({
-      cacheDir: defaultPuppeteerCacheDir,
-      browser: Browser.CHROME,
-      // TODO: It seems like this should be evaluated based on OS: https://github.com/berstend/chrome-versions
-      buildId: '122.0.6261.128',
-      unpack: true,
-    })
-  }
+  const dimensions = await page.evaluate(() => {
+    const pre = document.querySelector('pre')
+    if (pre === null) {
+      throw new Error(`<pre> element not found on the page`)
+    }
+
+    const preDOMRect = pre.getBoundingClientRect()
+    return {
+      width: preDOMRect.width,
+      height: preDOMRect.height,
+    }
+  })
+
+  await page.setViewport({
+    // NOTE: `width` and `height` are arbitrary numbers here because the `page.screenshot` method takes a screenshot of the entire <pre> element regardless of the dimensions of the page.
+    width: 100,
+    height: 100,
+    // TODO: Allow people to adjust image quality however they want.
+    deviceScaleFactor: 3,
+  })
+
+  await page.screenshot({
+    // TODO: Allow users save screenshots wherever they want. Maybe I need like 3 commands for that, 1 that will copy to clipboard, 1 that will save to path and 1 that will do both.
+    path: 'tmp/demo.jpeg',
+    // TODO: Maybe allow users to choose image type?
+    type: 'jpeg',
+    quality: 100,
+    captureBeyondViewport: true,
+    clip: {
+      x: 0,
+      y: 0,
+      width: dimensions.width,
+      height: dimensions.height,
+      // TODO: Not sure if scale is required here, but if so, it will probably have to be set to the same value as `eviceScaleFactor` in `setViewport` method.
+      scale: 3,
+    },
+  })
+
+  await browser.close()
 })()
