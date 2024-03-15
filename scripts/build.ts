5150d0c896e7ce1eb84f85dc5dcd13323ab2788f
ofadiman <ofadiman@gmail.com>
2024-03-15 17:50:50 +0100

    feature: configure environment to work with esmodules

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,8 +3,12 @@ import { build } from 'esbuild'
 build({
   entryPoints: ['src/screenshot.ts', 'src/install.ts'],
   bundle: true,
+  outExtension: {
+    '.js': '.cjs',
+  },
   outdir: 'bin',
   platform: 'node',
+  format: 'cjs',
 })
   .then(() => {
     console.info(`build succeed`)
